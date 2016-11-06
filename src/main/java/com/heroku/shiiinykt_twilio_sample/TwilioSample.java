10c9aabf23fbea07fc2c24e2a6bd16b48cd846ff
shiiinykt <shiiin.ykt@hoge.com>
2016-11-06 22:50:30 +0900

    UPDATE pebble

--- src/main/java/com/heroku/shiiinykt_twilio_sample/TwilioSample.java
+++ src/main/java/com/heroku/shiiinykt_twilio_sample/TwilioSample.java
@@ -2,10 +2,21 @@ package com.heroku.shiiinykt_twilio_sample;
 
 
 
+import com.mitchellbosecke.pebble.PebbleEngine;
+import com.mitchellbosecke.pebble.loader.ClasspathLoader;
+import com.mitchellbosecke.pebble.loader.Loader;
+import com.sun.xml.internal.ws.org.objectweb.asm.Label;
 import com.twilio.twiml.Say;
+import com.twilio.twiml.Say.Language;
 import com.twilio.twiml.VoiceResponse;
 
+import spark.ModelAndView;
+import spark.template.pebble.PebbleTemplateEngine;
+
 import static spark.Spark.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class TwilioSample {
@@ -15,13 +26,17 @@ public class TwilioSample {
 		 
 		 get("/index", (req, res) -> {
 			 res.type("application/xml");
-			 
-			 VoiceResponse voiceResponse = new VoiceResponse.Builder()
-		                .say(new Say.Builder("Hello Monkey").build())
-		                .build();
-			 return voiceResponse.toXml();
-		 });
+			 Map<String, Object> attributes = new HashMap<>();
+
+			 return new ModelAndView(attributes, "templates/index.peb");
+		 }, getTemplateEngine());
 
 	 }
+
+		public static PebbleTemplateEngine getTemplateEngine() {
+			Loader loader = new ClasspathLoader();
+			loader.setPrefix(null);
+			return new PebbleTemplateEngine(loader);
+		}
 
 }
