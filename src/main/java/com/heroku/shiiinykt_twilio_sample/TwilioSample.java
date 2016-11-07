9d33c74fc38ba12fd3da11e012f25963bb52a584
shiiinykt <shiiin.ykt@hoge.com>
2016-11-08 00:18:14 +0900

    UPDATE

--- src/main/java/com/heroku/shiiinykt_twilio_sample/TwilioSample.java
+++ src/main/java/com/heroku/shiiinykt_twilio_sample/TwilioSample.java
@@ -7,31 +7,54 @@ import com.mitchellbosecke.pebble.loader.Loader;
 
 import spark.ModelAndView;
 import spark.template.pebble.PebbleTemplateEngine;
+import spark.utils.StringUtils;
 
 import static spark.Spark.*;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 
 public class TwilioSample {
+
+	private static String MESSAGE_KEY = "message";
+	private static Map<String, String> MESSAGE_CACHE = new ConcurrentHashMap<>();
 	
-	 public static void main(final String[] args) {
-		 port(Integer.valueOf(System.getenv("PORT")));
-		 
-		 get("/index", (req, res) -> {
-			 res.type("application/xml");
-			 Map<String, Object> attributes = new HashMap<>();
+	public static void main(final String[] args) {
+		port(Integer.valueOf(System.getenv("PORT")));
 
-			 return new ModelAndView(attributes, "templates/index.peb");
-		 }, getTemplateEngine());
+		staticFileLocation("/public"); 
 
-	 }
+		get("/", (req, res) -> {
+			
+			return new ModelAndView(null, "templates/index.peb");
+		}, getTemplateEngine());
 
-		public static PebbleTemplateEngine getTemplateEngine() {
-			Loader loader = new ClasspathLoader();
-			loader.setPrefix(null);
-			return new PebbleTemplateEngine(loader);
-		}
+		post("/", (req, res) ->{
+
+			if(StringUtils.isNotEmpty(req.queryParams(MESSAGE_KEY))) {
+				MESSAGE_CACHE.put(MESSAGE_KEY, req.queryParams(MESSAGE_KEY));
+			}
+
+			res.redirect("/", 301);
+			return "";
+		});
+		
+		get("/callback", (req, res) -> {
+			res.type("application/xml");
+			Map<String, Object> attributes = new HashMap<>();
+			attributes.put(MESSAGE_KEY, MESSAGE_CACHE.get(MESSAGE_KEY));
+			
+			return new ModelAndView(attributes, "templates/callback.peb");
+		}, getTemplateEngine());
+
+	}
+
+	public static PebbleTemplateEngine getTemplateEngine() {
+		Loader loader = new ClasspathLoader();
+		loader.setPrefix(null);
+		return new PebbleTemplateEngine(loader);
+	}
 
 }
